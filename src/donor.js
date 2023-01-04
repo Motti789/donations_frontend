6341c6009658491a36f077114414595108dc0c32
Motti789 <mchillywilly@gmail.com>
2023-01-04 10:32:29 -0500

    created the donor class and added two static methods to get the html divs

---
+++ src/donor.js
@@ -0,0 +1,11 @@
+class Donor {
+
+  static all = []
+
+  static donorContainer = document.getElementById("donor-container")
+  static donorFormContainer = document.getElementById("donor-form-container")
+    
+
+
+
+}
